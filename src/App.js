35ef3b927c9645e5887e41de175a313928d54330
Matt Hall <matthall102@gmail.com>
2022-02-20 19:54:04 +0000

    Added copy to clipboard functionality

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, {Component} from "react";
 import "bootswatch/dist/minty/bootstrap.min.css";
 import './App.css';
 import WeaponForm from "./WeaponForm";
-import * as electron from 'electron'
 
 class App extends Component{
     render() {
